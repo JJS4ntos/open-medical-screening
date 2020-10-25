6d6af9a7eb37410e6522ffdfba68262dd576b73f
Junior Santos <jsantos.class@gmail.com>
2020-10-25 17:22:52 -0300

    feat(--): babel setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-export { ask } from "./doctor/index.js";
-export { removeTrashWords, trashWords } from "./config/trash_words.js";
-export {
+import { ask } from "./doctor/index.js";
+import { removeTrashWords, trashWords } from "./config/trash_words.js";
+import {
   getDiseasesFile,
   loadDiseaseSymptomAndPersist,
 } from "./persist/index.js";
+
+module.exports = {
+  ask,
+  removeTrashWords,
+  trashWords,
+  getDiseasesFile,
+  loadDiseaseSymptomAndPersist,
+};
